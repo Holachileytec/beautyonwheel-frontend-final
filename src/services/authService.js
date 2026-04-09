e441c491c6571929f180bc87e2938bc09eb18f8b
ibrahim fatai <writetoppy_2014@yahoo.com>
2026-04-09 01:41:07 +0100

    fix: correct forgot password API path

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,9 @@
-
 import api from "../config/api";
 
-const API = "http://localhost:5000/api/auth"; // <-- your backend base URL
-
 export const forgotPassword = (email) => {
-  return api.post(`${API}/forgot-password`, { email });
+  return api.post(`/api/users/forgot-password`, { email });
 };
 
 export const resetPassword = (token, password) => {
-  return api.post(`${API}/reset-password/${token}`, { password });
+  return api.post(`/api/users/reset-password/${token}`, { password });
 };
